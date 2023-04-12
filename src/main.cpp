87681b458b55091487385b3f36c41cde37256256
nudwix <nudwix@gmail.com>
2023-04-13 00:21:30 +0400

    Added simple shaders for rendering a pink triangle

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,52 @@
 #include <iostream>
+#include <string>
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+static unsigned int CompileShader(unsigned int type, const std::string& source) {
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    // Error handling
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+
+    if(result == GL_FALSE) {
+       int length;
+       glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+
+       char* message = static_cast<char*>(alloca(length * sizeof(char)));
+       glGetShaderInfoLog(id, length, &length, message);
+       std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << " shader!\n";
+       std::cout << message << '\n';
+       glDeleteShader(id);
+
+        return 0;
+    }
+
+    return id;
+}
+
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader) {
+    unsigned int program = glCreateProgram();
+
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+}
 
 int main() {
     GLFWwindow* window;
@@ -41,14 +87,32 @@ int main() {
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
 
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    std::string vertexShader = R"(
+            #version 330 core
+
+            layout(location = 0) in vec4 position;
+
+            void main() {
+               gl_Position = position;
+            })";
+
+    std::string fragmentShader = R"(
+            #version 330 core
+
+            layout(location = 0) out vec4 color;
+
+            void main() {
+               color = vec4(1.0, 0.0, 1.0, 1.0);
+            })";
+
+    unsigned int shader = CreateShader(vertexShader, fragmentShader);
+    glUseProgram(shader);
 
     // Loop until the user closes the window
     while (!glfwWindowShouldClose(window)) {
         // Render here
         glClear(GL_COLOR_BUFFER_BIT);
-
-//        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         // Swap front and back buffers
         glfwSwapBuffers(window);
@@ -56,6 +120,8 @@ int main() {
         // Poll for and process events
         glfwPollEvents();
     }
+
+    glDeleteProgram(shader);
 
     glfwTerminate();
     return 0;
